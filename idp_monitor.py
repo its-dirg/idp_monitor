ddaed3568c5253d089fe68d8344e53b364fde8d5
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-04-08 22:16:07 +0200

    Improved output ? Specially the Nagios output.

--- idp_monitor.py
+++ idp_monitor.py
@@ -13,7 +13,9 @@ from saml2.config import SPConfig, logging
 from saml2.s_utils import rndstr
 from saml2.samlp import STATUS_SUCCESS
 
-from interaction import Interaction, Discovery, JSRedirect
+from interaction import Interaction
+from interaction import Discovery
+from interaction import JSRedirect
 from interaction import InteractionNeeded
 from interaction import Action
 
@@ -121,6 +123,7 @@ class Check(object):
             else:
                 _last_action = _spec
 
+            login_start = 0
             try:
                 page_type = _spec["page-type"]
             except KeyError:
@@ -159,74 +162,108 @@ class Check(object):
                 raise
 
 
-NAGIOS_LINE = "[{time}] PROCESS_SERVICE_CHECK_RESULT;{host};{svc};{code};{" \
-              "output}"
+NAGIOS_LINE = ("[{time}] PROCESS_SERVICE_CHECK_RESULT;{host};{svc};{code};{"
+               "output}")
 
 RETURN_CODE = {"OK": 0, "WARNING": 1, "CRITICAL": 2, "UNKNOWN": 3}
+
+
+def print_nagios_line(return_code, nagios_args, output):
+    _kwargs = {
+        "time": time.time(),
+        "code": return_code,
+        "output": output
+    }
+    _kwargs.update(nagios_args)
+    print NAGIOS_LINE.format(**_kwargs)
+
+
+def print_status(resp, nagios, code, nagios_args, output, suppress_output,
+                 login_time):
+    if nagios:
+        print_nagios_line(RETURN_CODE[code], nagios_args, output)
+    else:
+        if code != "OK":
+            print code
+            if resp:
+                print >> sys.stderr, resp.response.status
+        elif not suppress_output:
+            if login_time:
+                print "OK %s" % login_time
+            else:
+                print "OK"
 
 
 def check(client, conf, entity_id, suppress_output=False, login_time=False,
           nagios=False, nagios_args=None):
 
-    check = Check(client, conf.INTERACTION)
+    try:
+        _check = Check(client, conf.INTERACTION)
+    except Exception, err:
+        print_status(None, nagios, "CRITICAL", nagios_args, "%s" % err,
+                     suppress_output, 0)
+        return RETURN_CODE["CRITICAL"]
 
-    _client = check.client
+    if login_time:
+        _login_time = _check.login_time
+    else:
+        _login_time = 0
+
+    _client = _check.client
     relay_state = rndstr()
     _id, htargs = _client.prepare_for_authenticate(entity_id,
                                                    relay_state=relay_state)
     resp = _client.send(htargs["headers"][0][1], "GET")
 
     if resp.status_code >= 400:
-        print "Error"
-        print >> sys.stderr, resp.text
-        return
+        print_status(resp, nagios, "CRITICAL", nagios_args,
+                     "HTTP status code: %d" % resp.status_code,
+                     suppress_output, _login_time)
+        return RETURN_CODE["CRITICAL"]
 
     # resp should be dictionary with keys RelayState, SAMLResponse and endpoint
     try:
-        resp = check.intermit(resp)
+        resp = _check.intermit(resp)
     except Exception, err:
-        print "ERROR: %s" % err
-        raise
+        print_status(None, nagios, "UNKNOWN", nagios_args, "%s" % err,
+                     suppress_output, _login_time)
+        return RETURN_CODE["UNKNOWN"]
     else:
         if resp is None:
             print "Error"
         else:
             serv, binding = _client.config.endpoint2service(resp["endpoint"])
 
-            resp = _client.parse_authn_request_response(resp["SAMLResponse"],
-                                                        binding)
-            assert resp.in_response_to == _id
+            try:
+                resp = _client.parse_authn_request_response(
+                    resp["SAMLResponse"], binding)
+            except Exception, err:
+                print_status(resp, nagios, "UNKNOWN", nagios_args, "%s" % err,
+                             suppress_output, _login_time)
+                return RETURN_CODE["UNKNOWN"]
+
+            try:
+                assert resp.in_response_to == _id
+            except AssertionError:
+                print_status(resp, nagios, "CRITICAL", nagios_args,
+                             "CRITICAL- Wrong ID in in_response_to",
+                             suppress_output, _login_time)
+                return RETURN_CODE["CRITICAL"]
+
             try:
                 assert resp.response.status.status_code.value == STATUS_SUCCESS
             except AssertionError:
                 # Got an error response
-                if nagios:
-                    _kwargs = {
-                        "time": time.time(),
-                        "code": RETURN_CODE["CRITICAL"],
-                        "output": resp.response.status
-                    }
-                    _kwargs.update(nagios_args)
-                    print NAGIOS_LINE.format(**_kwargs)
-                else:
-                    print "Error"
-                    print >> sys.stderr, resp.response.status
-                raise
+                print_status(resp, nagios, "CRITICAL", nagios_args,
+                             "CRITICAL- Unsuccessful authentication",
+                             suppress_output, _login_time)
+                return RETURN_CODE["CRITICAL"]
             else:
-                if nagios:
-                    _kwargs = {
-                        "time": time.time(),
-                        "code": RETURN_CODE["OK"],
-                        "output": ""
-                    }
-                    _kwargs.update(nagios_args)
-                    print NAGIOS_LINE.format(**_kwargs)
-                elif not suppress_output:
-                    if login_time:
-                        print "OK %s" % check.login_time
-                    else:
-                        print "OK"
+                print_status(resp, nagios, "OK", nagios_args,
+                             "OK- AUTH is working", suppress_output, login_time)
+                return RETURN_CODE["OK"]
 
+    return 0
 
 def main():
     parser = argparse.ArgumentParser()
@@ -279,11 +316,15 @@ def main():
 
     if args.count == "1":
         try:
-            check(client, conf, entity_id, login_time=args.login_split_time,
-                  nagios=args.nagios, nagios_args=nagios_args)
-        except Exception:
-            print "CRITICAL"
-            sys.exit(1)
+            status = check(client, conf, entity_id,
+                           login_time=args.login_split_time,
+                           nagios=args.nagios, nagios_args=nagios_args)
+        except Exception, err:
+            print_status(None, args.nagios, "UNKNOWN", nagios_args, "%s" % err,
+                         suppress_output=False, login_time=0)
+            sys.exit(3)
+        else:
+            sys.exit(status)
     else:
         for i in range(0, int(args.count)):
             check(client, conf, entity_id, suppress_output=True,
